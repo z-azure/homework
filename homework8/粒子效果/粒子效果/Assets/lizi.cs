0fc9e11807e1d9898b18cad7bdd4e5aaecf31e3e
z928468089 <928468089@qq.com>
2019-11-06 10:36:02 +0800

    hw7

---
+++ homework8/粒子效果/粒子效果/Assets/lizi.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curve
+{
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    private float z = 0.0f;
+
+    public float alpha, belta;
+
+    public float angle;
+
+    public Curve(float angle)
+    {
+        this.angle = angle;
+    }
+
+    public float getX()
+    {
+        return x;
+    }
+
+    public float getY()
+    {
+        return y;
+    }
+
+    public void Draw()
+    {
+        float t = angle / 180.0f * Mathf.PI;
+        float[] randomArr = { 0.4f, 0.5f, 0.6f, 0.7f };
+        float a = randomArr[Random.Range(0, randomArr.Length)];
+        x = a * (16 * Mathf.Pow(Mathf.Sin(t), 3));
+        y = a * (13 * Mathf.Cos(t) - 5 * Mathf.Cos(2 * t) - 2 * Mathf.Cos(3 * t) - Mathf.Cos(4 * t));
+    }
+
+
+    public void draw_3d(float changeangle)
+    {
+        float tempangle = changeangle / 180f * Mathf.PI;
+        float the = angle / 180f * Mathf.PI;
+        float p = 5f * Mathf.Sin(2f * the);
+        x = p * Mathf.Cos(the - tempangle);
+        y = p * Mathf.Sin(the - tempangle);
+    }
+}
---
+++ homework8/粒子效果/粒子效果/Assets/trail.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trail : MonoBehaviour {
+    public ParticleSystem particleSystem;
+    private ParticleSystem.Particle[] particleArray;
+    Curve[] particle;
+    // Use this for initialization
+    private int count = 500;
+    public float speed;
+
+	void Start () {
+        speed = 0.5f;
+        particleArray = new ParticleSystem.Particle[count];
+        particle = new Curve[count];
+        particleSystem.maxParticles = count;
+        particleSystem.Emit(count);
+        particleSystem.GetParticles(particleArray);
+        Init();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mod == true)
+        {
+            int i;
+            int level = 2;
+            for (i = 0; i < count; i++)
+            {
+                //外层粒子顺时针旋转
+                if (i % level > 0)
+                {
+                    particle[i].angle -= (i % level + 1) * speed;
+
+                }
+                else
+                {
+                    //内层逆时针旋转
+                    particle[i].angle += (i % level + 1) * speed;
+                }
+
+                particle[i].angle = (particle[i].angle + 360.0f) % 360.0f;
+                particle[i].Draw();
+                particleArray[i].position = new Vector3(particle[i].getX(), particle[i].getY(), 0.0f);
+            }
+        }
+        else
+        {
+
+            draw3d();
+        }
+
+        particleSystem.SetParticles(particleArray, particleArray.Length);
+    }
+
+    float begintheta = 0f;
+    public bool mod = false;
+
+    void draw3d()
+    {
+        float theta = begintheta;
+        for(int i = 0; i < count; i++)
+        {
+            particle[i].angle -= speed;
+            particle[i].draw_3d(theta);
+            theta += 0.5f;
+            theta %= 360f;
+            particleArray[i].position = new Vector3(particle[i].getX(), particle[i].getY(), 0.0f);
+        }
+        //begintheta += 2f;
+    }
+
+    private void Init()
+    {
+        int i;
+        for (i = 0; i < count; i++)
+        {
+            float angle = Random.Range(0.0f, 360.0f);
+            particle[i] = new Curve(angle);
+            //particle[i].Draw();
+            //particleArray[i].position = new Vector3(particle[i].getX(), particle[i].getY(), 0f);
+        }
+
+        particleSystem.SetParticles(particleArray, particleArray.Length);
+    }
+}
