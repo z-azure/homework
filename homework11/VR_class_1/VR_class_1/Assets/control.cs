c56ee5f4b97b518f3653097331d4d75ce8803675
z928468089 <928468089@qq.com>
2019-12-17 16:05:55 +0800

    hw11

---
+++ homework11/VR_class_1/VR_class_1/Assets/control.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class control : MonoBehaviour, IVirtualButtonEventHandler
+{
+    //public GameObject vb;
+    public Animator ani;
+    public VirtualButtonBehaviour[] vbs;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "vb_2")
+        {
+            ani.gameObject.transform.position += new Vector3(1, 0, 0);
+        }
+        else
+        {
+            ani.gameObject.transform.position += new Vector3(-1, 0, 0);
+        }
+        
+        Debug.Log(vb.VirtualButtonName);
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //Debug.Log("button2");
+        //throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        //VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        for(int i = 0; i < vbs.Length; i++)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
